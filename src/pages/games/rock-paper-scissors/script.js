ccb7bcee49c023368e20c0e10ffb9c495b12d1a3
DavidBonelo <bonelodev@gmail.com>
2023-09-07 17:49:46 -0500

    add ui to rock paper scissors game

--- src/pages/games/rock-paper-scissors/script.js
+++ src/pages/games/rock-paper-scissors/script.js
@@ -1,98 +1,89 @@
-const choices = ["rock", "scisors", "paper"];
+const selections = ["rock", "scissors", "paper"];
+const userScorePara = document.getElementById("user-score");
+const machineScorePara = document.getElementById("machine-score");
+const tieScorePara = document.getElementById("tie-score");
+const roundResultPara = document.getElementById("round-result");
 
-function getMachineChoice() {
+const selectionButtons = document.querySelectorAll("button.selection");
+selectionButtons.forEach((button) =>
+  button.addEventListener("click", userRoundChoose)
+);
+const resetButton = document.getElementById("reset-game");
+resetButton.addEventListener("click", resetGame);
+
+let userWinsCount = 0,
+  machineWinsCount = 0,
+  tiesCount = 0;
+
+function userRoundChoose(e) {
+  const userSelection = e.target.id;
+  const machineSelection = getMachineSelection();
+  console.log(`user: ${userSelection} machine: ${machineSelection}`);
+
+  const winner = playRound(userSelection, machineSelection);
+
+  updateScores(userSelection, machineSelection, winner);
+  displayResults();
+}
+
+function getMachineSelection() {
   let randomNumber = Math.floor(Math.random() * 3); // 0 - 2
-  return choices[randomNumber];
+  return selections[randomNumber];
 }
 
-function getUserChoice() {
-  let userChoice;
-  while (!choices.includes(userChoice)) {
-    userChoice = prompt("Rock, Paper or Scisors\nWhich one you choose?");
-    userChoice = userChoice?.toLowerCase();
-    if (!choices.includes(userChoice)) {
-      alert("Invalid choice, the options are: Rock, Scisors or Paper");
-    }
-  }
-  return userChoice;
-}
-
-function getRoundsAmmount() {
-  let roundsCount;
-  while (!roundsCount || roundsCount <= 0) {
-    roundsCount = Number(
-      prompt(
-        "GAME\nROCK - PAPER - SCISORS\n\nHow many rounds do you want to play?"
-      )
-    );
-    if (!roundsCount || roundsCount <= 0) {
-      alert("The ammount of rounds should be a number greater than 0");
-    }
-  }
-  return roundsCount;
-}
-
-function playRound(userChoice, machineChoice) {
+function playRound(userSelection, machineSelection) {
   let winner;
 
-  if (userChoice === machineChoice) {
+  if (userSelection === machineSelection) {
     winner = "tie";
-  } else if (userChoice === "rock") {
-    winner = machineChoice === "scisors" ? userChoice : machineChoice;
-  } else if (userChoice === "scisors") {
-    winner = machineChoice === "paper" ? userChoice : machineChoice;
-  } else if (userChoice === "paper") {
-    winner = machineChoice === "rock" ? userChoice : machineChoice;
+  } else if (userSelection === "rock") {
+    winner = machineSelection === "scissors" ? userSelection : machineSelection;
+  } else if (userSelection === "scissors") {
+    winner = machineSelection === "paper" ? userSelection : machineSelection;
+  } else if (userSelection === "paper") {
+    winner = machineSelection === "rock" ? userSelection : machineSelection;
   } else {
-    console.log("invalid choice");
+    console.log("invalid selection");
     return;
   }
   return winner;
 }
 
-function displayResults(userWinsCount, machineWinsCount, tiesCount) {
-  alert(
-    `The user won ${userWinsCount} games!
-The machine won ${machineWinsCount} games!
-${tiesCount} games resulted in a tie!`
-  );
-  if (userWinsCount === machineWinsCount) {
-    alert("Nobody won, its a tie!");
+function updateScores(userSelection, machineSelection, winner) {
+  if (winner === userSelection) {
+    userWinsCount++;
+    roundResultPara.innerText = `${userSelection} beats ${machineSelection}, the user wins this round!`;
+  } else if (winner === machineSelection) {
+    machineWinsCount++;
+    roundResultPara.innerText = `${machineSelection} beats ${userSelection}, the user wins this round!`;
   } else {
-    alert(
-      `The ${
-        userWinsCount > machineWinsCount ? "user" : "machine"
-      } wins the game!`
-    );
+    tiesCount++;
+    roundResultPara.innerText = `${userSelection} vs ${machineSelection} ends in a tie!!`;
+  }
+  if (userWinsCount === 5 || machineWinsCount === 5) {
+    gameOver();
   }
 }
 
-function startGame() {
-  const roundsCount = getRoundsAmmount();
-  console.log("Total rounds: " + roundsCount);
-  let userWinsCount = 0,
-    machineWinsCount = 0,
-    tiesCount = 0;
-  for (let i = 0; i < roundsCount; i++) {
-    const userChoice = getUserChoice();
-    const machineChoice = getMachineChoice();
-    console.log(`Round ${i + 1} user: ${userChoice} machine: ${machineChoice}`);
-    console.table({
-      round: i + 1,
-      user: `${userChoice}`,
-      machine: `${machineChoice}`,
-    });
-
-    const winner = playRound(userChoice, machineChoice);
-    if (winner === userChoice) {
-      userWinsCount++;
-    } else if (winner === machineChoice) {
-      machineWinsCount++;
-    } else {
-      tiesCount++;
-    }
-  }
-  displayResults(userWinsCount, machineWinsCount, tiesCount);
+function gameOver() {
+  roundResultPara.classList.add("game-over");
+  roundResultPara.innerText = `The ${
+    userWinsCount > machineWinsCount ? "user" : "machine"
+  } wins the game!`;
+  selectionButtons.forEach((button) => (button.disabled = true));
 }
 
-startGame();
+function displayResults() {
+  userScorePara.innerText = `User score: ${userWinsCount}`;
+  machineScorePara.innerText = `Machine score: ${machineWinsCount}`;
+  tieScorePara.innerText = `Ties count: ${tiesCount}`;
+}
+
+function resetGame() {
+  tiesCount = 0;
+  userWinsCount = 0;
+  machineWinsCount = 0;
+  roundResultPara.classList.remove("game-over");
+  roundResultPara.innerText = "First player to score 5 wins!";
+  selectionButtons.forEach((button) => (button.disabled = false));
+}
